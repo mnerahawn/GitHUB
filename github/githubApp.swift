3742864882161834a310e7ed2fa2887da3fad174
mnerahawn <mnerahawncs@gmail.com>
2022-11-02 10:52:40 +0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by mnerah awn on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
